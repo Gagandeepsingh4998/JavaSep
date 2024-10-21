9e4ab0bf8cd62ac405523ae783b9c610dd918d8f
gagandeepsingh4998 <gagandeepsingh8488@gmail.com>
2024-10-21 16:44:09 -0400

    hello this is the new program

--- src/new1.java
+++ src/new1.java
@@ -1,5 +1,7 @@
 public class new1 {
     public static void main(String[] args) {
         System.out.println("this is the new file here");
+        System.out.println("Hello");
+        System.out.println("This is the new method");
     }
 }
