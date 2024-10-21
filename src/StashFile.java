0a9a2098e95b77155c21e8b586ba788352da3419
gagandeepsingh4998 <gagandeepsingh8488@gmail.com>
2024-10-21 15:56:53 -0400

    This is the new file

---
+++ src/StashFile.java
@@ -0,0 +1,7 @@
+public class StashFile {
+    public static void main(String[] args) {
+        System.out.println("This is the stash file in the java");
+        System.out.println(" ");
+    }
+}
+
